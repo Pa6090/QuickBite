0e438955cc15efe5d3b872c9845ab3f2b107a04e
Pa6090 <pavanmittapalli1@gmail.com>
2024-05-03 12:19:42 +0530

    Resolved cors

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,12 @@ const port = process.env.PORT;
 const app = express()
 
 app.use(cors({
-            origin : [""],
+            origin : ["http://localhost:3000"],
             methods : ["POST", "GET"],
             credentials : true
         })
 )
+
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
     res.header(
