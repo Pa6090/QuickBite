437491606f899f161c8a038e7da17d518be29ea0
Pa6090 <pavanmittapalli1@gmail.com>
2024-05-03 15:12:24 +0530

    Added package.json

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,13 @@ app.use(cors({
         })
 )
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://quick-bite-theta.vercel.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-Allow-Origin","https://quick-bite-theta.vercel.app/");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 app.use(express.json())
 app.use(cookieParser())
