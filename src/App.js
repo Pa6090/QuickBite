479058745c5ba274f817f3243842026dc1ce5734
Pa6090 <pavanmittapalli1@gmail.com>
2024-05-03 11:12:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/screens/Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/screens/Login.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import Signup from './components/screens/Signup.js';
+import CartProvider from './components/ContextReducer.js';
+import Orders from './components/screens/Orders.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/signup' element={<Signup />} />
+            <Route exact path='/myOrder' element={<Orders />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+
   );
 }
 
